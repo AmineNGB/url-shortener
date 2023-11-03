9cf950680f4650c5c83e1a8653c9eb6897f91ce1
Amine NEGHBEL <amineneghbel@gmail.com>
2023-11-03 19:29:08 +0100

    adding shortcode decode with base 62

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -13,10 +13,17 @@ class ShortCode
       result.prepend char
       number = number / BASE
     end
+
     result
   end
 
   def self.decode(string)
-
+    number = 0
+    string.reverse.each_char.with_index do |char, index|
+      power = BASE**index
+      index = ALPHABET.index(char)
+      number += index * power
+    end
+    number
   end
 end
